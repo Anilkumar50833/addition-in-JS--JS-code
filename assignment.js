0d3d6f3f18bd0028e1bcf780c217341b3d8649f6
ANILKUMAR KUNTRAPAKAM <115290903+Anilkumar50833@users.noreply.github.com>
2022-10-07 23:21:27 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,6 @@
+function add(){
+    var a=document.getElementById('t1').value;
+    var b=document.getElementById('t2').value;
+    var c=parseInt(a)+parseInt(b);
+    document.getElementById('t3').value=c;
+}
